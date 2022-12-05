eff28bf6a82f8820b5c8b5faa8ee787c71d1e05e
RaviStimphil <97135286+RaviStimphil@users.noreply.github.com>
2022-12-04 18:35:19 -0600

    Cards visual feedback
    It's easier to know that more infomation can be pressed.

--- src/components/Card.js
+++ src/components/Card.js
@@ -59,14 +59,14 @@ export default function Card(props) {
                         {props.deadline}
                     </p>
                 </div>
-                <div className="bg-amber-300 cursor-pointer flex justify-center border-amber-300 font-bold pl-3
+                <div className="bg-amber-300 hover:bg-amber-200 cursor-pointer flex justify-center border-amber-300 font-bold pl-3
                 text-center border-b-2 rounded-b-xl" onClick={handleMoreInfoClick}>
                     More Information
                 </div>
             </div> :
             // Ternary operator second section
             <div className="flex flex-col text-center text-gray-600 h-full">
-                <div className="grid font-bold pl-3 grid-cols-2 text-left mb-0 border-b-2">
+                <div className="grid font-bold pl-3 grid-cols-2 text-left mb-0 border-b-2 border-amber-300">
                     Quest Creator: {" "}
                     <p className="ml-1 font-normal">
                         {props.creator}
@@ -79,11 +79,11 @@ export default function Card(props) {
                     {props.description}
                 </p>
                 <div className="grid grid-cols-2 mt-auto h-[28px]">
-                    <button className="h-[26px] flex bg-gray-200 cursor-pointer font-bold pl-3
-                    border-b-2 rounded-bl-xl" onClick={handleMoreInfoClick}>
+                    <button className="h-[26px] flex bg-amber-300 cursor-pointer font-bold pl-3
+                     rounded-bl-xl hover:bg-amber-200" onClick={handleMoreInfoClick}>
                         Less Information
                     </button>
-                    <button onClick={acceptQuest}  className="border-b-2 bg-green-200 rounded-br-xl font-bold">
+                    <button onClick={acceptQuest}  className="border-b-2 border-yellow-100 bg-green-200 hover:bg-green-100 rounded-br-xl font-bold">
                         Accept Quest
                     </button>
                 </div>
